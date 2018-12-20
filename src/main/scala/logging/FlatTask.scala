89d8de0ce2d94edc47fc730c4f7be466eb7091a6
brenton <brenton.d.walker@gmail.com>
2018-12-20 11:26:22 +0100

    add toString methods to case classes and replace immutable objects with mutable where appropriate

--- src/main/scala/logging/FlatTask.scala
+++ src/main/scala/logging/FlatTask.scala
@@ -4,4 +4,9 @@ case class FlatTask(taskId: Long, stageId: Long, host: String, local: Boolean,
                     sojournTime: Double, waitingTime:Double, serviceTime: Double,
                     deserializationTime: Double, schedulerOverhead: Double,
                     runtime: Double, readTime:Double) {
+  override def toString = {
+    ("taskId:"+taskId+" stageId:"+stageId+" host:"+host+" local:"+local+" sojournTime:"+sojournTime 
+      +" waitingTime:"+waitingTime+" serviceTime:"+serviceTime+" deserializationTime:"+deserializationTime
+      +" schedulerOverhead:"+schedulerOverhead+"runtime:"+runtime)
+  }
 }
